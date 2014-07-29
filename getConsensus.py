0603b0215ca89598d36cd966fb5f472882c1a55a
mr664 <m.ravenhall@live.co.uk>
2014-07-29 19:51:17 +0100

    Updated getConsensus.py

--- getConsensus.py
+++ getConsensus.py
@@ -2,26 +2,37 @@ def fastaToMatrix(fasta):
 	#given a fasta, returns a matrix as:
 	#matrix = [[seqA], [seqB], [seqC]]
 	
-	seqcatch = ""
+	strains = []
+	strains.append([])
 	location = -1
 	f = open(fasta, "r")
-	
+
+        #create empty matrix
+
+	#populate matrix
 	for line in f: #take name from > to /n, seq from /n to >
 	#currently fastaToDict, needs to be ToMatrix
 		if ">" in line:
 			location += 1
-			seqcatch = ""
+			#add matrix position here? strains[0][location]
 		else:
-			seqcatch += line.rstrip()
-		strains[0][location] = seqcatch
+                        #add seq to the previously created position
+			strains[location].append(line.rstrip())
+		
 	f.close()
-	return(strains)	
+	return(strains)
+
+        #alt: capture each sequence in a list, then create matrix from that list
 
 def findConsensus(fasta):
 	base = ['A', 'C', 'G', 'T']
 	count = 0
 
 	matrix = fastaToMatrix(fasta)
+
+	#TEST: print matrix
+        #for row in matrix:
+                #print(' '.join(row))
 
 	#make profile matrix from matrix
 	#make consensus string from profile matrix
@@ -30,3 +41,4 @@ def findConsensus(fasta):
 	for x, y in profile:
 		print(base[count], x, y)
 		count += 1
+
